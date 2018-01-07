0b61d13634c16188cf1dbf4de8545092a89ce44c
Gitaek kwon <break@kimsq.com>
2018-01-07 11:53:55 +0900

    회원모듈 기본테마명 변경 및 기본값 셋팅

---
+++ modules/member/themes/_mobile/rc-simple/login/main.php
@@ -0,0 +1,15 @@
+<form>
+	<div class="form-list stacked">
+		<div class="input-row">
+			<label>아이디</label>
+			<input type="text" name="id" placeholder="아이디를 입력하세요.">
+		</div>
+		<div class="input-row">
+			<label>패스워드</label>
+			<input type="password" name="pw" placeholder="패스워드를 입력하세요.">
+		</div>
+		<div class="content-padded">
+			<button type="button" class="btn btn-primary btn-lg btn-block">로그인</button>
+		</div>
+	</div>
+</form>
