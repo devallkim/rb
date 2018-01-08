3dedc77831c6e2cb47ecf717612eab3adecf606a
Gitaek kwon <break@kimsq.com>
2018-01-08 14:34:23 +0900

    게시판모듈  mod 폴더 추가

---
+++ modules/bbs/mod/_permcheck.php
@@ -0,0 +1,16 @@
+<div id="guidebox">
+<div class="tit"><h3>서비스 안내</h3></div>
+
+	<div class="guide_box">
+		<ul>
+		<li>요청하신 페이지는 권한이 있어야 접근하실 수 있습니다.</li>
+		<li>로그인하신 후에 이용하세요.</li>
+		<li>로그인을 하신 후에도 이 페이지가 출력되면 회원등급 권한이 없는 경우입니다.</li>
+		</ul>
+
+		<div class="member_btm">
+			<input type="button" value="돌아가기" class="btn_m btn_cnfrm" onclick="history.back();" />
+		</div>
+	</div>
+
+</div>
