ea095b3f35e63feec8805ebd00c52ba15532536d
Gitaek kwon <break@kimsq.com>
2018-01-08 12:32:14 +0900

    게시판 모듈 초기 셋팅 파일 추가

---
+++ modules/bbs/var/var.php
@@ -0,0 +1,18 @@
+<?php
+$d['bbs']['skin_main'] = "_desktop/bs4-default";
+$d['bbs']['skin_mobile'] = "_mobile/rc-default";
+$d['bbs']['skin_total'] = "_desktop/bs4-default";
+$d['bbs']['rss'] = "";
+$d['bbs']['restr'] = "";
+$d['bbs']['replydel'] = "";
+$d['bbs']['commentdel'] = "";
+$d['bbs']['badword'] = "";
+$d['bbs']['badword_action'] = "0";
+$d['bbs']['badword_escape'] = "";
+$d['bbs']['singo_del'] = "";
+$d['bbs']['singo_del_num'] = "";
+$d['bbs']['singo_del_act'] = "1";
+$d['bbs']['recnum'] = "20";
+$d['bbs']['sbjcut'] = "40";
+$d['bbs']['newtime'] = "24";
+?>
