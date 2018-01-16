811d66e0bd9e779228e71869257c335526ca4828
Gitaek kwon <break@kimsq.com>
2018-01-16 12:12:37 +0900

    회원,게시판 모듈의 joint 시 $r 값 제거
    모듈 연결이후, 사이트 코드가 변경되어 $r 값이 변경된 경우 문제가 발생하기 때문에 아예 모듈 연결시에 $r 을 제거

--- modules/member/admin/var/var.joint.php
+++ modules/member/admin/var/var.joint.php
@@ -3,20 +3,19 @@
 	<div class="card-body">
 
 		<p>
-			회원관리 모듈은 <strong>회원가입/로그인/마이페이지</strong>를 포함하고 있습니다.<br>
 			연결할 페이지를 선택해 주세요.
 		</p>
 
-		<button type="button" class="btn btn-light" onclick="dropJoint('<?php echo $g['s']?>/?r=<?php echo $r?>&m=<?php echo $smodule?>&front=join');">
+		<button type="button" class="btn btn-light" onclick="dropJoint('<?php echo $g['s']?>/?m=<?php echo $smodule?>&front=join');">
 			회원가입
 		</button>
-		<button type="button" class="btn btn-light" onclick="dropJoint('<?php echo $g['s']?>/?r=<?php echo $r?>&m=<?php echo $smodule?>&front=login');">
+		<button type="button" class="btn btn-light" onclick="dropJoint('<?php echo $g['s']?>/?m=<?php echo $smodule?>&front=login');">
 			로그인
 		</button>
-		<button type="button" class="btn btn-light" onclick="dropJoint('<?php echo $g['s']?>/?r=<?php echo $r?>&m=<?php echo $smodule?>&front=profile');">
+		<button type="button" class="btn btn-light" onclick="dropJoint('<?php echo $g['s']?>/?m=<?php echo $smodule?>&front=profile');">
 			프로필
 		</button>
-		<button type="button" class="btn btn-light" onclick="dropJoint('<?php echo $g['s']?>/?r=<?php echo $r?>&m=<?php echo $smodule?>&front=settings');">
+		<button type="button" class="btn btn-light" onclick="dropJoint('<?php echo $g['s']?>/?m=<?php echo $smodule?>&front=settings');">
 			개인정보 관리
 		</button>
 	</div>
