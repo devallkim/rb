a1c666269c38d03e848005fd9f56c1bc518f56c0
Gitaek kwon <break@kimsq.com>
2018-01-18 13:16:28 +0900

    마켓 시작하기 페이지 추가

---
+++ modules/market/admin/start.php
@@ -0,0 +1,98 @@
+<section class="p-4">
+  <p class="text-muted">kimsQ Rb2를 사용해 주셔서 감사합니다. 처음 사용하시는 분들을 위해 4가지 사이트 패키지가 준비되어 있습니다. 설치하여 체험해 보시고 활용방법에 대해 참고해 주세요.</p>
+  <hr>
+
+  <div class="card-deck">
+    <div class="card">
+      <a href="#"><img class="card-img-top" src="http://placehold.it/500x250" alt="회사소개형 A"></a>
+      <div class="card-body">
+        <h5 class="card-title"><a class="muted-link d-block" href="#">회사소개형 A</a><small class="text-muted">(전구간 반응형)</small></h5>
+        <p class="card-text text-muted">데스크탑~모바일 구간의 반응형 웹을 지원하는 기업소개형 웹사이트 패키지 입니다.</p>
+      </div>
+      <div class="card-footer">
+        <button type="button" class="btn btn-outline-primary btn-block">설치하기 (준비중)</button>
+      </div>
+    </div>
+    <div class="card">
+      <a href="#"><img class="card-img-top" src="http://placehold.it/500x250" alt="회사소개형 B"></a>
+      <div class="card-body">
+        <h5 class="card-title"><a class="muted-link d-block" href="#">회사소개형 B</a> <small class="text-muted">(데스크탑+모바일전용)</small></h5>
+        <p class="card-text text-muted">데스크탑 전용사이트와 모바일 전용사이트로 구분된 패키지 입니다. 모바일 전용사이트에서는 스마트폰~태블릿 구간의 반응형을 지원합니다.</p>
+      </div>
+      <div class="card-footer">
+        <button type="button" class="btn btn-outline-primary btn-block">설치하기 (준비중)</button>
+      </div>
+    </div>
+
+    <div class="card">
+      <a href="#"><img class="card-img-top" src="http://placehold.it/500x250" alt="서비스형 A"></a>
+      <div class="card-body">
+        <h5 class="card-title"><a class="muted-link d-block" href="#">서비스형 A</a> <small class="text-muted">(데스크탑+모바일전용)</small></h5>
+        <p class="card-text text-muted">간단한 회원가입이 포함된 커뮤니티형 웹사이트 입니다.</p>
+      </div>
+      <div class="card-footer">
+        <button type="button" class="btn btn-outline-primary btn-block">설치하기 (준비중)</button>
+      </div>
+    </div>
+    <div class="card">
+      <a href="#"><img class="card-img-top" src="http://placehold.it/500x250" alt="메뉴얼형 A"></a>
+      <div class="card-body">
+        <h5 class="card-title"><a class="muted-link d-block" href="#">메뉴얼형 A</a> <small class="text-muted">(데스크탑+모바일전용)</small></h5>
+        <p class="card-text text-muted">마크다운 전용에디터를 사용하여 구조화된 글쓰기를 지원합니다. 모바일에서도 최적화된 메뉴얼 컨텐츠 사이트를 구축하실수 있습니다.</p>
+      </div>
+      <div class="card-footer">
+        <button type="button" class="btn btn-outline-primary btn-block">설치하기 (준비중)</button>
+      </div>
+    </div>
+  </div>
+
+<p class="text-muted mt-3"><small>더 다양한 패키지가 궁금하신가요? 프로젝트를 <span class="text-info">스탠다드 플랜</span>으로 업그레이드 하시면 킴스큐 코아팀에서 제공하는 더 다양한 패키지가 설치할수 있습니다.</small></p>
+
+
+  <h5 class="h6 mt-5 mb-3 text-muted">Rb2 파워 개발자가 되기 위한 첫걸음 !!</h5>
+  <div class="rb-extension-thumbnails text-center mt-4">
+    	<div class="row">
+    		<div class="col-sm-6 col-md-3">
+    			<div class="thumbnail">
+    				<i class="rb-icon fa-4x kf-module"></i>
+    				<div class="caption mt-3">
+    					<h5>Rb2 개발</h5>
+    					<small class="text-muted">Rb2 파워 개발자가 되기 위한 첫걸음 !! Rb2는 매우 직관적인 폴더구조를 가지고 있습니다.</small>
+    					<p><a class="btn btn-light mt-2" href="http://kimsq.com/docs/rb2" role="button" target="_blank">자세히보기</a></p>
+    				</div>
+    			</div>
+    		</div>
+    		<div class="col-sm-6 col-md-3">
+    			<div class="thumbnail">
+    				<i class="rb-icon fa-4x fa fa-mobile"></i>
+    				<div class="caption mt-3">
+    					<h5>모바일 프론트 개발</h5>
+    					<small class="text-muted">작은 화면과 터치기반 인터페이스에 최적화된, 모바일 네이티브앱의 디자인과 사용자 경험을 제공합니다.</small>
+    					<p><a class="btn btn-light mt-2" href="http://kimsq.com//docs/rc" role="button" target="_blank">자세히보기</a></p>
+    				</div>
+    			</div>
+    		</div>
+    		<div class="col-sm-6 col-md-3">
+    			<div class="thumbnail">
+    				<i class="rb-icon fa-4x fa fa-desktop"></i>
+    				<div class="caption mt-3">
+    					<h5>데스크탑 프론트 개발</h5>
+    					<small class="text-muted">Rb2의 데스크탑에는 전세계에서 가장 널리 사용되는 프런트엔드 컴포넌트 라이브러리인 부트스트랩 v4가 적용되어 있습니다.</small>
+    					<p><a class="btn btn-light mt-2" href="http://kimsq.com//docs/bs4" role="button" target="_blank">자세히보기</a></p>
+    				</div>
+    			</div>
+    		</div>
+    		<div class="col-sm-6 col-md-3">
+    			<div class="thumbnail">
+    				<i class="rb-icon fa-4x fa fa-android"></i>
+    				<div class="caption mt-3">
+    					<h5>하이브리드앱 개발</h5>
+    					<small class="text-muted">Rb2로 제작된 모바일웹을 하이브리드 앱으로 제작 및 배포하는 방법을 코르도바와 폰갭 중심으로 안내 합니다.</small>
+    					<p><a class="btn btn-light mt-2" class="btn btn-light" href="http://kimsq.com//docs/cd" role="button" target="_blank">자세히보기</a></p>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+
+</section>
