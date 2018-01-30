b7c86d4616c9d757c8156b9ce18ce82776d0d8dc
Gitaek kwon <break@kimsq.com>
2018-01-30 12:37:44 +0900

    관리자 사이드 패널 우측상단 아바타 이미지 관련 수정

--- _core/engine/adminpanel.engine.php
+++ _core/engine/adminpanel.engine.php
@@ -76,7 +76,7 @@ $g['wcache'] = $d['admin']['cache_flag']?'?nFlag='.$date[$d['admin']['cache_flag
 					<?php if ($my['photo']): ?>
 					<img class="rounded-circle" data-role="avatar" src="<?php echo $g['s']?>/_core/opensrc/timthumb/thumb.php?src=/_var/avatar/<?php echo $my['photo']?>&w=50&h=50&s=1" width="25">
 					<?php else: ?>
-					<img class="rounded-circle bg-faded" data-role="avatar" src="<?php echo $g['s']?>/_var/avatar/0.svg" width="25">
+					<img class="rounded-circle" data-role="avatar" src="<?php echo $g['s']?>/_var/avatar/0.svg" width="25">
 					<?php endif; ?>
 					<span><?php echo $my[$_HS['nametype']]?></span>
 					<small id="rb-notification-name"></small>
