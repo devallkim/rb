ea325847091757f0de72ad75ab4b8c2db7224c0b
Gitaek kwon <break@kimsq.com>
2018-04-30 08:11:28 +0900

    팝업 모듈 개선작업

--- modules/popup/action/a.regispopup.php
+++ modules/popup/action/a.regispopup.php
@@ -8,8 +8,8 @@ $term2 = $year2.$month2.$day2.$hour2.$min2.'00';
 $name = trim(strip_tags($name));
 
 if ($uid) {
-	$QVAL = "hidden='".$hidden."',term0='".$term0."',term1='".$term1."',term2='".$term2."',name='".$name."',content='".$source."',html='".$html."',upload='".$upload."',center='".$center."',";
-	$QVAL.= "ptop='".$ptop."',pleft='".$pleft."',width='".$width."',height='".$height."',scroll='".$scroll."',type='".$type."',dispage='".$dispage."',theme='".$theme."'";
+	$QVAL = "hidden='".$hidden."',admin='".$admin."',term0='".$term0."',term1='".$term1."',term2='".$term2."',name='".$name."',content='".$source."',m_content='".$m_source."',html='".$html."',m_html='".$m_html."',upload='".$upload."',m_upload='".$m_upload."',position='".$position."',center='".$center."',";
+	$QVAL.= "ptop='".$ptop."',pleft='".$pleft."',width='".$width."',draggable='".$draggable."',type='".$type."',m_type='".$m_type."',dispage='".$dispage."',skin='".$skin."',m_skin='".$m_skin."'";
 
 	getDbUpdate($table['s_popup'],$QVAL,'uid='.$uid);
 	setrawcookie('result_popup_main', rawurlencode('팝업내용이 수정되었습니다.|success'));  // 처리여부 cookie 저장
@@ -17,8 +17,8 @@ if ($uid) {
 
 } else {
 
-	$QKEY = "hidden,term0,term1,term2,name,content,html,upload,center,ptop,pleft,width,height,scroll,type,dispage,theme";
-	$QVAL = "'$hidden','$term0','$term1','$term2','$name','$source','$html','$upload','$center','$ptop','$pleft','$width','$height','$scroll','$type','$dispage','$theme'";
+	$QKEY = "hidden,admin,term0,term1,term2,name,content,m_content,html,m_html,upload,m_upload,position,center,ptop,pleft,width,draggable,type,m_type,dispage,skin,m_skin";
+	$QVAL = "'$hidden','$admin','$term0','$term1','$term2','$name','$source','$m_source','$html','$m_html','$upload','$m_upload','$position','$center','$ptop','$pleft','$width','$draggable','$type','$m_type','$dispage','$skin','$m_skin'";
 
 	getDbInsert($table['s_popup'],$QKEY,$QVAL);
 	$lastpopup = getDbCnt($table['s_popup'],'max(uid)','');
