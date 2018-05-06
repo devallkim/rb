3ff4db3b9d8f8b7c0becca7fcf4344bf5c8a0857
Gitaek kwon <break@kimsq.com>
2018-05-07 07:04:55 +0900

    rc-bbs-media-01 위젯 더보기 헤더 추가

--- widgets/bbs/rc-bbs-media-01/main.php
+++ widgets/bbs/rc-bbs-media-01/main.php
@@ -13,29 +13,39 @@ $g['dir_module_skin'] = $g['path_module'].'bbs/themes/'.$d['bbs']['skin'].'/';
 include_once $g['dir_module_skin'].'_widget.php';
 ?>
 
-<ul class="widget table-view table-view-full" data-role="bbs-list">
-
-  <?php $_RCD=getDbArray($table['bbsdata'],($wdgvar['bid']?'bbs='.$B['uid'].' and ':'').'display=1 and site='.$_HS['uid'],'*','gid','asc',$wdgvar['limit'],1)?>
-  <?php while($_R=db_fetch_array($_RCD)):?>
-  <li class="table-view-cell media" id="item-<?php echo $_R['uid'] ?>">
-    <a role="button" id="item-<?php echo $_R['uid'] ?>"
-      href="#modal-bbs-view" data-toggle="modal"
-      data-bid="<?php echo $wdgvar['bid'] ?>"
-      data-uid="<?php echo $_R['uid'] ?>"
-      data-url="<?php echo getBbsPostLink($_R)?>"
-      data-cat="<?php echo $_R['category'] ?>"
-      data-title="<?php echo $wdgvar['title']?>"
-      data-subject="<?php echo $_R['subject'] ?>">
-      <img class="media-object pull-left" src="<?php echo getPreviewResize(getUpImageSrc($_R),$size) ?>" style="width: <?php echo $print_width ?>px">
-      <div class="media-body">
-        <?php if(getNew($_R['d_regis'],24)):?>
-        <small class="rb-new mr-1" aria-hidden="true"></small>
-        <?php endif?>
-         <?php echo $_R['subject']?>
-        <p><?php echo $_R['category']?></p>
-      </div>
+<section class="widget">
+  <header class="d-flex justify-content-between align-items-center p-2">
+    <strong><?php echo $wdgvar['title']?></strong>
+    <?php if($wdgvar['link']):?>
+    <a href="<?php echo $wdgvar['link']?>" class="muted-link small">
+      더보기 <i class="fa fa-angle-right" aria-hidden="true"></i>
     </a>
-  </li>
-  <?php endwhile?>
+    <?php endif?>
+  </header>
+  <ul class="widget table-view table-view-full" data-role="bbs-list">
 
-</ul>
+    <?php $_RCD=getDbArray($table['bbsdata'],($wdgvar['bid']?'bbs='.$B['uid'].' and ':'').'display=1 and site='.$_HS['uid'],'*','gid','asc',$wdgvar['limit'],1)?>
+    <?php while($_R=db_fetch_array($_RCD)):?>
+    <li class="table-view-cell media" id="item-<?php echo $_R['uid'] ?>">
+      <a role="button" id="item-<?php echo $_R['uid'] ?>"
+        href="#modal-bbs-view" data-toggle="modal"
+        data-bid="<?php echo $wdgvar['bid'] ?>"
+        data-uid="<?php echo $_R['uid'] ?>"
+        data-url="<?php echo getBbsPostLink($_R)?>"
+        data-cat="<?php echo $_R['category'] ?>"
+        data-title="<?php echo $wdgvar['title']?>"
+        data-subject="<?php echo $_R['subject'] ?>">
+        <img class="media-object pull-left" src="<?php echo getPreviewResize(getUpImageSrc($_R),$size) ?>" style="width: <?php echo $print_width ?>px">
+        <div class="media-body">
+          <?php if(getNew($_R['d_regis'],24)):?>
+          <small class="rb-new mr-1" aria-hidden="true"></small>
+          <?php endif?>
+           <?php echo $_R['subject']?>
+          <p><?php echo $_R['category']?></p>
+        </div>
+      </a>
+    </li>
+    <?php endwhile?>
+
+  </ul>
+</section>
