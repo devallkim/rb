daf358b99cdbd290292859edefb4d853aaccbac2
Gitaek kwon <break@kimsq.com>
2018-06-29 08:15:31 +0900

    사이트 로그인/로그아웃 기능개선- 휴대폰,이메일 로그인/ 소셜로그인 계정통합
    [로그인]
    휴대폰,이메일로 로그인 지원
    소셜로그인시 계정통합 처리(소셜로그인을 했는데, 동일한 이메일(본인인증완료) 이 존재할때)
    
    [로그아웃]
    회원정보테이블(s_mbrdata)에서 sns 필드는 현재 로그인중인 sns 임. 그래서 로그아웃 시에 해당 필드를 비워줌

--- modules/site/action/a.login.php
+++ modules/site/action/a.login.php
@@ -7,12 +7,15 @@ $pw	= trim($_POST['pw']);
 
 if (!$id || !$pw) getLink('reload','parent.','이메일과 패스워드를 입력해 주세요.',$history);
 
-if (strpos($id,'@') && strpos($id,'.'))
-{
-	$M1 = getDbData($table['s_mbrdata'],"email='".$id."'",'*');
-	$M	= getUidData($table['s_mbrid'],$M1['memberuid']);
-}
-else {
+if (strpos($id,'@') && strpos($id,'.')) {
+	$M2 = getDbData($table['s_mbremail'],"email='".$id."'",'*');
+	$M	= getUidData($table['s_mbrid'],$M2['mbruid']);
+	$M1 = getDbData($table['s_mbrdata'],'memberuid='.$M['uid'],'*');
+} else if (substr($id,0,3)=='010' || substr($id,0,3)=='011' || substr($id,0,3)=='016' || substr($id,0,3)=='017' || substr($id,0,3)=='018' || substr($id,0,3)=='019') {
+	$M2 = getDbData($table['s_mbrphone'],"phone='".$id."'",'*');
+	$M	= getUidData($table['s_mbrid'],$M2['mbruid']);
+	$M1 = getDbData($table['s_mbrdata'],'memberuid='.$M['uid'],'*');
+} else {
 	$M = getDbData($table['s_mbrid'],"id='".$id."'",'*');
 	$M1 = getDbData($table['s_mbrdata'],'memberuid='.$M['uid'],'*');
 }
@@ -27,8 +30,9 @@ if (!$M['uid'] || $M1['auth'] == 4) {
 	exit();
 }
 if ($M1['auth'] == 2) getLink('reload','parent.','회원님은 인증보류 상태입니다.',$history);
-if ($M1['auth'] == 3) getLink('reload','parent.','회원님은 이메일 인증대기 상태입니다.',$history);
-if ($M['pw'] != getCrypt($pw,$M1['d_regis']) && $M1['tmpcode'] != $pw) {
+if ($M1['auth'] == 3) getLink('reload','parent.','회원님은 본인 인증대기 상태입니다.',$history);
+
+if ($M['pw'] != getCrypt($pw,$M1['d_regis']) && $M1['tmpcode'] != getCrypt($pw,$M1['d_regis'])) {
   echo "<script>";
 	echo "parent.$('".$form."').removeClass('was-validated');";
 	echo "parent.$('".$form."').find('[type=submit]').prop('disabled', false);";
@@ -38,12 +42,56 @@ if ($M['pw'] != getCrypt($pw,$M1['d_regis']) && $M1['tmpcode'] != $pw) {
 	exit();
 }
 
-
 if ($usertype == 'admin')
 if (!$M1['admin']) getLink('reload','parent.','회원님은 관리자가 아닙니다.',$history);
 
 getDbUpdate($table['s_mbrdata'],"tmpcode='',num_login=num_login+1,now_log=1,last_log='".$date['totime']."'",'memberuid='.$M['uid']);
 getDbUpdate($table['s_referer'],'mbruid='.$M['uid'],"d_regis like '".$date['today']."%' and site=".$s." and mbruid=0 and ip='".$_SERVER['REMOTE_ADDR']."'");
+
+//소셜 로그인 후, 계정통합 처리
+if ($snsuid && $snsname) {
+	$d_regis	= $date['totime'];
+
+	// SNS에 등록된 아바타가 있고, 회원정보에 저장된 아바타가 없을 경우
+	if (!$M['photo'] && $_photo) {
+		if (strpos($_photo,'facebook'))
+		{
+			$_facebookHeaders = get_headers($_photo, 1);
+			if (array_key_exists('Location', $_facebookHeaders))
+			{
+				$_photo = $_facebookHeaders['Location'];
+				$_photodata = getCURLData($_photo,'');
+			}
+		}
+		else {
+			$_photodata = getCURLData($_photo,'');
+		}
+
+		if ($_photodata)
+		{
+			$fileExt	= 'jpg';
+			$fp = fopen($g['path_var'].'avatar/snstmp.jpg','w');
+			fwrite($fp,$_photodata);
+			fclose($fp);
+
+			$photo		= $id.'.'.$fileExt;
+			$saveFile1	= $g['path_var'].'avatar/'.$photo;
+
+			include $g['path_core'].'function/thumb.func.php';
+			ResizeWidth($g['path_var'].'avatar/snstmp.jpg',$saveFile1,360);
+
+			@chmod($saveFile1,0707);
+			unlink($g['path_var'].'avatar/snstmp.jpg');
+		}
+		getDbUpdate($table['s_mbrdata'],"photo='".$photo."'",'memberuid='.$M['uid']);  // 회원정보 아바타 이미지 저장
+	}
+
+	getDbUpdate($table['s_mbrdata'],"sns='".$snsname."'",'memberuid='.$M['uid']);  // 회원정보 sns 로그인 기록
+	getDbUpdate($table['s_mbremail'],"d_verified='".$d_regis."'",'mbruid='.$M['uid'].' and email="'.$id.'"');  //  // 소셜미디어에서 전달받은 이메일 본인확인 처리
+
+	getDbInsert($table['s_mbrsns'],'mbruid,sns,id,access_token,refresh_token,expires_in,d_regis',"'".$M['uid']."','".$snsname."','$snsuid','$sns_access_token','$sns_refresh_token','$sns_expires_in','$d_regis'");
+	$_SESSION['SL'] = ''; // 소셜로그인 세션 초기화
+}
 
 if($login_cookie=='checked'){
  setAccessToken($M1['memberuid'],'login');
@@ -79,6 +127,6 @@ if ($M1['admin']) {
 	if ($g['mobile']&&$_SESSION['pcmode']!='Y') getLink($referer?$referer:$g['s'].'/?r='.$r,'parent.','','');
 	getLink($g['s'].'/?r='.$r.'&panel=Y&_admpnl_='.urlencode($referer),'parent.','','');
 }
-setrawcookie('site_login_result', rawurlencode('로그인 되었습니다.|default'));  // 알림처리를 위한 로그인 상태 cookie 저장
+setrawcookie('site_login_result', rawurlencode($M1['name'].'님 로그인 되었습니다.|default'));  // 알림처리를 위한 로그인 상태 cookie 저장
 getLink($referer?$referer:$g['s'].'/?r='.$r,'parent.','','');
 ?>
--- modules/site/action/a.logout.php
+++ modules/site/action/a.logout.php
@@ -3,7 +3,7 @@ if(!defined('__KIMS__')) exit;
 
 if ($my['uid'])
 {
-	getDbUpdate($table['s_mbrdata'],'now_log=0','memberuid='.$my['uid']);
+	getDbUpdate($table['s_mbrdata'],"now_log=0,sns=''",'memberuid='.$my['uid']);
 	$_SESSION['mbr_uid'] = '';
 	$_SESSION['mbr_logout'] = '1';
 	setAccessToken($my['uid'],'logout'); // 토큰 데이타 삭제 및 쿠키 초기화
