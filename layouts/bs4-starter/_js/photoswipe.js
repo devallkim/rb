12c1cb42b328d5d633f530464257697738e247ef
Gitaek kwon <break@kimsq.com>
2018-10-01 16:13:29 +0900

    포토스와이프 Bounds 설정 비활성 처리함
    셈네일의 비율과 원본이미지의 비율이 다를 경우, Bounds 효과를 적용하면 부자연스러움
    섬네일의 비율과 원본의 비율이 다른 경우가 대부분 이기 때문.

--- layouts/bs4-starter/_js/photoswipe.js
+++ layouts/bs4-starter/_js/photoswipe.js
@@ -147,14 +147,6 @@ var initPhotoSwipeFromDOM = function(gallerySelector) {
 					// define gallery index (for URL)
 					galleryUID: galleryElement.getAttribute('data-pswp-uid'),
 
-					getThumbBoundsFn: function(index) {
-							// See Options -> getThumbBoundsFn section of documentation for more info
-							var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
-									pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
-									rect = thumbnail.getBoundingClientRect();
-
-							return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
-					}
 
 			};
 
